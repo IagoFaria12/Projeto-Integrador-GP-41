5eb31552bab7fbb6e5622b1bc7da09c1420831cd
Leandro Morais <leandromorais1221@gmail.com>
2026-04-28 23:35:17 -0300

    chore: update schema database

---
+++ data/schema.sql
@@ -0,0 +1,31 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "categories" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "products" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL UNIQUE,
+	"price"	REAL NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "regions" (
+	"id"	INTEGER,
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "sales" (
+	"id"	INTEGER,
+	"quantity_sold"	INTEGER NOT NULL,
+	"rating"	NUMERIC NOT NULL,
+	"date"	TEXT NOT NULL,
+	"category_id"	INTEGER NOT NULL,
+	"product_id"	INTEGER NOT NULL,
+	"region_id"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	CONSTRAINT "fk_categories" FOREIGN KEY("category_id") REFERENCES "categories"("id"),
+	CONSTRAINT "fk_products" FOREIGN KEY("product_id") REFERENCES "products",
+	CONSTRAINT "fk_regions" FOREIGN KEY("region_id") REFERENCES ""
+);
+COMMIT;
