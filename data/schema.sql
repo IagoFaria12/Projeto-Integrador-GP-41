8662b074a529bc4e8c4b624a0d41bbd0d6669e11
Leandro Morais <leandromorais1221@gmail.com>
2026-04-30 21:55:54 -0300

    chore: resolve database inconsistencies

--- data/schema.sql
+++ data/schema.sql
@@ -7,7 +7,9 @@ CREATE TABLE IF NOT EXISTS "categories" (
 CREATE TABLE IF NOT EXISTS "products" (
 	"id"	INTEGER,
 	"price"	REAL NOT NULL,
-	PRIMARY KEY("id")
+	"category_id"	INTEGER NOT NULL,
+	PRIMARY KEY("id"),
+	CONSTRAINT "fk_categories" FOREIGN KEY("category_id") REFERENCES ""
 );
 CREATE TABLE IF NOT EXISTS "regions" (
 	"id"	INTEGER,
@@ -17,13 +19,12 @@ CREATE TABLE IF NOT EXISTS "regions" (
 CREATE TABLE IF NOT EXISTS "sales" (
 	"id"	INTEGER,
 	"quantity_sold"	INTEGER NOT NULL,
+	"discount_percent" NUMERIC NOT NULL,
 	"rating"	NUMERIC NOT NULL,
 	"date"	TEXT NOT NULL,
-	"category_id"	INTEGER NOT NULL,
 	"product_id"	INTEGER NOT NULL,
 	"region_id"	INTEGER NOT NULL,
 	PRIMARY KEY("id"),
-	CONSTRAINT "fk_categories" FOREIGN KEY("category_id") REFERENCES "categories"("id"),
 	CONSTRAINT "fk_products" FOREIGN KEY("product_id") REFERENCES "products",
 	CONSTRAINT "fk_regions" FOREIGN KEY("region_id") REFERENCES ""
 );
