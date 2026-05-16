27ad30c6412450588b6661aaee2e0f562ac05648
Felipe Alencar <feclp208gmail.com>
2026-05-15 21:35:42 -0300

    chore: update queries

--- business/queries.py
+++ business/queries.py
@@ -326,3 +326,171 @@ def gross_price_range_distribution(current_year = None, first_month = None, last
     conn.close()
 
     return rows
+
+def gross_total_revenue(categories = None, current_year = None, first_month = None, last_month = None):
+
+    conn, cursor = get_connection()
+
+    sql = """
+    SELECT 
+         ROUND(SUM(p.price * s.quantity_sold),2 ) AS total_gross_revenue
+    FROM sales s
+    INNER JOIN products p
+    ON s.product_id = p.id
+    """
+
+    params, sql = filter_year_month(sql,categories, current_year, first_month, last_month)
+
+    cursor.execute(sql, (params))
+    rows = cursor.fetchone()
+    
+    cursor.close()
+    conn.close()
+
+    return rows
+
+def total_revenue_with_discount(categories = None, current_year = None, first_month = None, last_month = None):
+
+    conn, cursor = get_connection()
+
+    sql = """
+    SELECT 
+    ROUND(SUM((p.price - (p.price * s.discount_percent / 100.0)) * s.quantity_sold),2) AS total_revenue
+    FROM sales s
+    INNER JOIN products p
+    ON s.product_id = p.id
+
+    """
+
+    params, sql = filter_year_month(sql,categories, current_year, first_month, last_month)
+
+    cursor.execute(sql, (params))
+    rows = cursor.fetchone()
+    
+    cursor.close()
+    conn.close()
+
+    return rows
+
+def total_number_of_orders(categories = None, current_year = None, first_month = None, last_month = None):
+
+    conn, cursor = get_connection()
+
+    sql = """
+    SELECT 
+    COUNT(s.id) AS quantity_total_orders
+    FROM sales s;
+
+    """
+
+    params, sql = filter_year_month(sql,categories, current_year, first_month, last_month)
+
+    cursor.execute(sql, (params))
+    rows = cursor.fetchone()
+    
+    cursor.close()
+    conn.close()
+
+    return rows
+
+def average_evaluation_of_products(categories = None, current_year = None, first_month = None, last_month = None):
+
+    conn, cursor = get_connection()
+
+    sql = """
+    SELECT
+    ROUND((SUM(s.rating)) / COUNT(s.rating), 1) AS average_product_evaluation
+    FROM sales AS s 
+
+    """
+
+    params, sql = filter_year_month(sql,categories, current_year, first_month, last_month)
+
+    cursor.execute(sql, (params))
+    rows = cursor.fetchone()
+    
+    cursor.close()
+    conn.close()
+
+    return rows
+
+def avarege_tickets(categories=None, current_year=None, first_month=None, last_month=None):
+
+    conn, cursor = get_connection()
+
+    sql = """
+    WITH ticket_average AS (
+        SELECT 
+            p.price,
+            s.quantity_sold,
+            s.discount_percent,
+
+            ROUND(
+                (p.price - (p.price * s.discount_percent / 100.0)) 
+                * s.quantity_sold,
+                2
+            ) AS total_order_revenue
+
+        FROM sales AS s
+
+        INNER JOIN products AS p
+            ON s.product_id = p.id
+    ),
+
+    grupos AS (
+        SELECT
+            total_order_revenue,
+
+            NTILE(3) OVER (
+                ORDER BY total_order_revenue
+            ) AS ticket_group
+
+        FROM ticket_average
+    ),
+
+    categorized_sales AS (
+        SELECT 
+            total_order_revenue,
+
+            CASE 
+                WHEN ticket_group = 1 THEN 'low cost'
+                WHEN ticket_group = 2 THEN 'medium cost'
+                ELSE 'high cost'
+            END AS category
+
+        FROM grupos
+    )
+
+    SELECT 
+        category,
+        COUNT(*) AS quantity_orders,
+
+        ROUND(
+            AVG(total_order_revenue),
+            2
+        ) AS average_ticket
+
+    FROM categorized_sales
+    """
+
+    # params, sql = filter_year_month(
+    #     sql,
+    #     categories,
+    #     current_year,
+    #     first_month,
+    #     last_month
+    # )
+
+    sql += """
+    GROUP BY category
+    ORDER BY average_ticket ASC;
+    """
+
+    cursor.execute(sql)
+
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return rows
